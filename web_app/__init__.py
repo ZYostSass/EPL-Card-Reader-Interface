e09fafc609fbd6dd978688377d73d84bdd8cad46
Mikayla Maki <mikayla.c.maki@gmail.com>
2023-02-22 15:49:58 -0800

    Integrated database, template, and web app code Improved run script to execute migrations Added instructions for how to modify the database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,2 +1,9 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
