8be43d96d38a78a8b90dab366d8898aa66bc45b6
natasha <nnaylor@pdx.edu>
2023-04-20 10:58:06 -0700

    Starting login page, needs more styling

--- web_app/views.py
+++ web_app/views.py
@@ -4,6 +4,11 @@ from . import db
 from . import app
 
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
