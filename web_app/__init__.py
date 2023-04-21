d225b76ac57e27384f61fdbee7f754902e6d899c
frosty <gleas3@pdx.edu>
2023-04-20 19:03:29 -0700

    Think I have a non-blocking reader working.  Need to debug the formatting again, I think

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from card_reader.reader import CardReader
 from threading import Thread
+import queue
 
 
 app = Flask(__name__)
@@ -12,10 +13,23 @@ db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
 
+q = queue.Queue()
 ser = CardReader(port='/dev/ttyUSB0', baud_rate=9600)
-def start_reader():
-    ser.read_serial()
 
 
-thread = Thread(target=start_reader)
-thread.start()# Call the above function when the app starts
+def read_serial(q):
+    while True:
+        card_number, facility_code = ser.read_serial()
+        if card_number and facility_code:
+            q.put((card_number, facility_code))
+
+
+thread = Thread(target=read_serial, args=(q,))
+thread.start()
+
+while True:
+    try:
+        card_number, facility_code = q.get(timeout=1)
+        print(f"Card Number: {card_number}, Facility Code: {facility_code}")
+    except queue.Empty:
+        continue
