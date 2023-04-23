a510c879e4e9f3a7e26e6780dfbbda15f86d136d
frosty <gleas3@pdx.edu>
2023-04-23 09:16:06 -0700

    Transposed the string slice

--- card_reader/reader.py
+++ card_reader/reader.py
@@ -23,7 +23,7 @@ class CardReader:
                         test_ct += 1 # Added this to verify loop is actually executing each turn
                         print(test_ct)
                         continue
-                    clean = data[:2]
+                    clean = data[2:]
                     clean_int = int(clean, 16)
                     card_number = (clean_int >> 1)  & 0x7FFFF # Bitshift to remove parity and mask to isolate 19 bits
                     facility_code = (clean_int >> 20)  & 0xFFFF # Same for facility code
--- card_reader/test.py
+++ card_reader/test.py
@@ -1,3 +1,29 @@
-from card_reader.reader import serial_data
+import serial
+import threading
 
-serial_data()
+print("Testing card reader in single thread...")
+
+try:
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+    while True:
+        try:
+            data = ser.readline().decode().strip()
+            if not data:
+                print("No data read!")
+                continue
+            if data:
+                clean = data[:2]
+                clean_int = int(clean, 16)
+                card_number = (clean_int >> 1)  & 0x7FFFF # Bitshift to remove parity and mask to isolate 19 bits
+                facility_code = (clean_int >> 20)  & 0xFFFF # Same for facility code
+                print(f"Card number: {card_number}") # Verified when tested with my badge
+                print(f"Facility Code: {facility_code}") 
+                print("Card reader loop will now exit and close port for the single thread test.")
+                ser.close()
+                break
+        except Exception as e:
+            print(f"Error reading data from serial port: {str(e)}")
+            continue
+except Exception as e:
+            print("Error opening port: " + str(e))
+
