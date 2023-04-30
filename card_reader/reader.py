ca30df4af271c5d431ecbedbb44944a8c15e5dea
frosty <gleas3@pdx.edu>
2023-04-30 08:43:41 -0700

    Fixed facility code formatting

--- card_reader/reader.py
+++ card_reader/reader.py
@@ -11,10 +11,12 @@ class CardReader:
     def get_data(self):
       if self.ser.in_waiting > 0:
         data = self.ser.readline().decode().strip()
-        clean = data[2:]
+        clean = data[4:]
         clean_int = int(clean, 16)
         card_number = (clean_int >> 1)  & 0x7FFFF # Bitshift to remove parity and mask to isolate 19 bits
         facility_code = (clean_int >> 20)
+        print(f"Card: " + str(card_number))
+        print(f"Fac: " + str(facility_code))
         self.ser.reset_input_buffer()
         return (card_number, facility_code)
       else:
