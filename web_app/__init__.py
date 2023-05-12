232b86ec1bf58a68192ed6c884f00dc8e4dac5a0
Austin Gallup <agallup@pdx.edu>
2023-05-11 21:53:50 -0700

    updating branch before starting work

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from card_reader.reader import CardReader
 from flask_seeder import FlaskSeeder
 import os
 
@@ -9,6 +10,7 @@ import os
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dev"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 
 try:
   os.makedirs(app.instance_path)
@@ -16,6 +18,9 @@ except OSError:
   pass
 
 db = SQLAlchemy()
+
+# Commented out until card reader fix for other OS
+#card_reader = CardReader(baud_rate=9600) # Note- add option for timeout prefs?
 migrate = Migrate()
 seeder = FlaskSeeder()
 
