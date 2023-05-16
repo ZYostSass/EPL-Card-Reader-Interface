2db54b4239ef3ea548965a22b83fef145c8b788d
Austin Gallup <agallup@pdx.edu>
2023-05-15 18:51:02 -0700

    log.py had basic framework for running a persisting CSV log. needs to have a way to easily view file. and needs access to database.

---
+++ card_reader/log.py
@@ -0,0 +1,36 @@
+import csv
+import datetime 
+
+
+filename = 'log.csv'
+
+#run this when appending data to logfile. 
+def logFile(data):
+   with open('log.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    #fake data
+    fakedata = ['fname', 'lname', 'psuid', 'userid', data,  datetime.date.today()]
+    
+    #need to add call to database to grab correct info, then create writable object for writer. 
+    writer.writerow(fakedata)
+        
+
+
+#run this if the log file is empty        
+def initLog():
+    field = ['fname' , 'lname', 'PSUID', 'USERID', 'CardNumber', 'date/time']
+    
+    exampleData = ['John', 'Doe', '123123', '123123', '123123', 'datetime' ] 
+    
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(field)
+        csvwriter.writerow(exampleData)
+        
+def main():
+    print("Hello World!", datetime.date.today())
+    #initLog()
+    logFile('12345')
+
+if __name__ == "__main__":
+    main()
