ce25ae6e64b44e0d5a3b01cce22a81385e89321d
Mikayla Maki <mikayla.c.maki@gmail.com>
2023-05-19 22:29:24 -0700

    Add current_role template context

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 #from card_reader.reader import CardReader
@@ -37,3 +37,10 @@ app.register_blueprint(admin_bp)
 from .views import bp as views_bp
 
 app.register_blueprint(views_bp)
+
+@app.context_processor
+def add_current_role():
+    if g.user is not None:
+       return dict(current_role=g.user.role)
+    else: 
+      return dict(current_role=None)
