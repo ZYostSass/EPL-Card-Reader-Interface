bdfe07e38f0abb04075edab9fd5c0731b61e09b2
natasha <nnaylor@pdx.edu>
2023-05-28 10:03:08 -0700

    Fixed flash messages positioning to not be hidden behind navbar. Removed unused code. Adjusted styling for equipment color border for true and false

--- web_app/static/script.js
+++ web_app/static/script.js
@@ -1,6 +1,3 @@
-// Add color overlay on equipment images
-$('ul.list-unstyled > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
-
 // Collapsible sidebar
 $('.open-btn').on('click', function () {
   $('.sidebar').addClass('active');
