f7b334e742056453311755f5fd63fa32cc125e11
Mikayla Maki <mikayla.c.maki@gmail.com>
2023-05-28 18:26:50 -0700

    Add code for converting badge numbers fully

--- web_app/static/script.js
+++ web_app/static/script.js
@@ -1,72 +1,73 @@
-// Collapsible sidebar
-$('.open-btn').on('click', function () {
-  $('.sidebar').addClass('active');
-});
-$('.close-btn').on('click', function () {
-  $('.sidebar').removeClass('active');
-});
+$(function () {
+  // Collapsible sidebar
+  $('.open-btn').on('click', function () {
+    $('.sidebar').addClass('active');
+  });
+  $('.close-btn').on('click', function () {
+    $('.sidebar').removeClass('active');
+  });
 
-// Login window popup
-const center = document.querySelector('.center');
-const loginPopup = document.querySelector('.btn.btn-primary.login-popup');
-
-loginPopup.addEventListener('click', () => {
-  if (center.classList.contains('active-popup')) {
-    closePopup(center);
+  // Login window popup
+  const center = document.querySelector('.center');
+  const loginPopup = document.querySelector('.btn.btn-primary.login-popup');
+  if (loginPopup != null) {
+    loginPopup.addEventListener('click', () => {
+      if (center.classList.contains('active-popup')) {
+        closePopup(center);
+      }
+      else {
+        openPopup(center);
+      }
+    });
   }
-  else {
-    openPopup(center);
+
+  // Student checkin popup
+  const studentCheckin = document.querySelector(".student-checkin-content");
+  if (studentCheckin != null) {
+    if (document.URL.includes('/student-checkin')) {
+      if (studentCheckin.classList.contains('active-popup')) {
+        closePopup(studentCheckin);
+      }
+      else {
+        openPopup(studentCheckin);
+      }
+    }
+
+    studentCheckin.addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
   }
-});
 
 
-// Student checkin popup
-const studentCheckin = document.querySelector(".student-checkin-content");
-
-document.addEventListener('DOMContentLoaded', function() {
-  if (document.URL.includes('/student-checkin')) {
-    if (studentCheckin.classList.contains('active-popup')) {
+  // Student Checkin Close
+  const iconClose = document.querySelector('.icon-close')
+  if (iconClose != null) {
+    iconClose.addEventListener('click', function (event) {
       closePopup(studentCheckin);
-    }
-    else {
-      openPopup(studentCheckin);
-    }
+    });
   }
-});
 
+  if (center != null) {
+    center.addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
+  }
 
-// Student Checkin Close
-const iconClose = document.querySelector('.icon-close')
+  function openPopup(popup) {
+    popup.classList.add('active-popup');
+    setTimeout(function () {
+      popup.classList.remove('hidden');
+    }, 300);
+  }
 
-iconClose.addEventListener('click', function(event) {
-  closePopup(studentCheckin);
-});
+  function closePopup(popup) {
+    popup.classList.remove('active-popup');
+    setTimeout(function () {
+      popup.classList.add('hidden');
+    }, 300);
+  }
 
-center.addEventListener('click', function(event) {
-  event.stopPropagation();
-});
-
-studentCheckin.addEventListener('click', function(event) {
-  event.stopPropagation();
-});
-
-function openPopup(popup) {
-  popup.classList.add('active-popup');
-  setTimeout(function() {
-    popup.classList.remove('hidden');
-  }, 300);
-}
-
-function closePopup(popup) {
-  popup.classList.remove('active-popup');
-  setTimeout(function() {
-    popup.classList.add('hidden');
-  }, 300);
-}
-
-
-$(document).ready(function() {
-  document.getElementById("scan-btn").addEventListener("click", function(event) {
+  $("#scan-btn").on("click", function (event) {
     event.preventDefault();  // Prevent default link behavior
     fetch("/card_data/")  // Make the request to the card_data route
       .then(response => response.json())  // Parse the JSON response
